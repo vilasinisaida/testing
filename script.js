e67df31b349b5e589809008e947241501ea6bc7d
unknown <vilasini.modem@gmail.com>
2020-02-13 11:59:44 +0100

    paragraph script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient")
+var body = document.getElementById("gradient");
+var par = document.querySelector("p");
 
 function setGradient(){
 	body.style.background = "linear-gradient(to right," + color1.value +"," +color2.value+")";
